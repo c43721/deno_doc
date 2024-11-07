2a66a96de373eb7f873830c2297902f9b33ce838
c43721 <55610086+c43721@users.noreply.github.com>
2024-11-07 12:00:03 -0600

    feat: dont underscore numbers when formatting module names

--- src/html/usage.rs
+++ src/html/usage.rs
@@ -8,7 +8,7 @@ use regex::Regex;
 use serde::Serialize;
 
 lazy_static! {
-  static ref IDENTIFIER_RE: Regex = Regex::new(r"[^a-zA-Z$_]").unwrap();
+  static ref IDENTIFIER_RE: Regex = Regex::new(r"[^a-zA-Z1-9$_]").unwrap();
 }
 
 fn render_css_for_usage(name: &str) -> String {
